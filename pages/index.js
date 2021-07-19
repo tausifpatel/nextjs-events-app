eeb7a525f175b03bb8dc008c0e1d12457b3a30d3
Tausif <tausifpatel.dev@gmail.com>
2021-07-19 21:38:03 +0100

    Initialise commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Title from "../components/Title";
+import Header from "../components/Header";
+
+
+export default function Home() {
+
+  return (
+    <div>
+      <Header />
+      <Title />
+    </div>
+  );
+}
