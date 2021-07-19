9dae46b30a41fd9c80d135a18bfde1e60c89ccf2
Tausif <tausifpatel.dev@gmail.com>
2021-07-19 23:51:15 +0100

    Add Title.js

---
+++ components/Title.js
@@ -0,0 +1,13 @@
+const Title = () => {
+    return (
+        <div className="govuk-width-container ">
+            <main className="govuk-main-wrapper " id="main-content" role="main">
+                <h1 className="govuk-heading-xl heading">Tech events in London</h1>
+                <h2>Here's a look at some Tech events near London. </h2>
+            </main>
+        </div>
+    )
+}
+
+export default Title;
+
