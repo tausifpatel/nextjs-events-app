aab4e108ba476124f91ff83b9069c53efcc468a7
Tausif <tausifpatel.dev@gmail.com>
2021-07-19 23:46:57 +0100

    Add Title.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import Title  from "../components/Title";
 
 export default function Home() {
 
   return (
     <div>
       <Header />
+      <Title />
     </div>
   );
 }
