a7b58dde240eba2eee1434ce35320a46ace74b22
Tausif <tausifpatel.dev@gmail.com>
2021-07-22 11:59:01 +0100

    MAdd await keyword to eventsCollection,insertOne(data) to handle error caused due connection getting closed before data being inserted in db

--- pages/api/add-new-event.js
+++ pages/api/add-new-event.js
@@ -1,27 +1,49 @@
 import { MongoClient } from "mongodb";
 
-async function handles (req, res){
+async function handles(req, res){
     if(req.method === "POST"){
         const data = req.body;
 
-        const uri = "mongodb+srv://tausifpatel:KQ3jIhLzoFmncHQJ@cluster0.akm73.mongodb.net/events?retryWrites=true&w=majority";
+        const client = await MongoClient.connect("mongodb+srv://tausifpatel:Zzz1dW1NRWpnTfJr@cluster0.akm73.mongodb.net/events?retryWrites=true&w=majority");
+        
+        const db = client.db();  // Alternatively client.db("events") - where "events is db name"
 
-        const client = new MongoClient(uri);
+        const eventsCollection = db.collection("events");
 
-        const clientConnected = await client.connect();
+        const result = await eventsCollection.insertOne(data);
 
-        const database = clientConnected.db();
-
-        const eventsCollection = database.collection("events");
-
-        const result = eventsCollection.insertOne(data);
-
-        console.log(result)
+        console.log(result);
 
         client.close();
 
-        res.status(201).json({message: "Event inserted!"});
+        res.status(201).json({message: "Meetup inserted!"});
     }
 }
+
+
+////////// From documentation 
+// async function handles(req, res){
+//     if(req.method === "POST"){
+//         const data = req.body;
+
+//         const uri = "mongodb+srv://tausifpatel:Zzz1dW1NRWpnTfJr@cluster0.akm73.mongodb.net/events?retryWrites=true&w=majority";
+
+//         const client = new MongoClient(uri);
+
+//         const clientConnected = await client.connect();
+
+//         const database = clientConnected.db("events");
+
+//         const eventsCollection = database.collection("events");
+
+//         const result = await eventsCollection.insertOne(data);
+
+//         console.log(result)
+
+//         client.close();
+
+//         res.status(201).json({message: "Event inserted!"});
+//     }
+// }
 
 export default handles;
--- pages/new-event.js
+++ pages/new-event.js
@@ -3,10 +3,24 @@ import Footer from "../components/Footer";
 import NewEventForm from "../components/NewEventForm";
 
 const NewEventPage = () => {
+
+    const handleAddNewEvent = async (eventData) => {
+        const response = await fetch('/api/add-new-event', {
+            method: "POST",
+            body: JSON.stringify(eventData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+        console.log(data);
+    }
+
     return (
         <div>
             <Header />
-            <NewEventForm />
+            <NewEventForm onAddEvent={handleAddNewEvent} />
             <Footer />
         </div>
     )
